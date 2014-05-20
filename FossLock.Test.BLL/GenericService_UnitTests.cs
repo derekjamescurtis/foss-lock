8ecb0aeb2e5474af6077750b8eef325f42eb7dd4
Derek J. Curtis <djcurtis@summersetsoftware.com>
2014-05-20 13:52:39 +0700

    Refactored out common mock initialization.  Added tests to make sure underlying repository is only being called as appropriate.

--- FossLock.Test.BLL/GenericService_UnitTests.cs
+++ FossLock.Test.BLL/GenericService_UnitTests.cs
@@ -9,52 +9,78 @@ using Moq;
 
 namespace FossLock.Test.BLL
 {
+
+    /*
+     * TODO: data retrieval tests
+     */
     [TestClass]
     public class GenericService_UnitTests
     {
+        #region Test Setup
+
+        GenericService<Product> productService = null;
+
+        // keeps track of the number of times these methods are called on productRepository
+        Dictionary<string, int> productRepoCalls = new Dictionary<string, int> 
+        { 
+            { "Add", 0 },
+            { "Update", 0 },
+            { "Delete", 0 }
+        };
+
         [TestInitialize]
-        public void SetUp() { }
+        public void SetUp() 
+        { 
+            var mockProductRepo = new Mock<IRepository<Product>>();
+            mockProductRepo
+                .Setup(e => e.Add(It.IsAny<Product>()))
+                .Callback(() => { productRepoCalls["Add"] += 1; })
+                .Returns((Product entity) =>
+                {                    
+                    entity.Id = 1; // give this an ID so it registers as 'non transient'
+                    return entity;
+                });
+            mockProductRepo
+                .Setup(e => e.Update(It.IsAny<Product>()))
+                .Callback(() =>  { productRepoCalls["Update"] += 1; })
+                .Returns((Product entity) => { return entity; });
+            mockProductRepo
+                .Setup(e => e.Delete(It.IsAny<Product>()))
+                .Callback(() => { productRepoCalls["Delete"] += 1; });
 
 
-        [TestMethod]
-        [Description("GenericService<T> constructor should never accept null arguments.")]
-        public void Constructor_NullRepository_ThrowsException()
-        {
-            try
-            {
-                var productService = new GenericService<Product>(null);
-                Assert.Fail("Exception was expected from constructor when passed null argument.");
-            }
-            catch (ArgumentNullException ex)
-            {
-                Assert.AreEqual("repository", ex.ParamName);
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail("Unexpected exception type: {0}\nMessage: {1}", ex.GetType(), ex.Message);
-            }
+            
+            // we want our service to call all of it's real methods
+            // they're all marked as virtual so we have to explicitally tell moq to call them.
+            var mockProductService = new Mock<GenericService<Product>>(mockProductRepo.Object);
+            mockProductService.Setup(e => e.New()).CallBase();
+
+            mockProductService.Setup(e => e.Add(It.IsAny<Product>())).CallBase();
+            mockProductService.Setup(e => e.Update(It.IsAny<Product>())).CallBase();
+
+            // TODO: blah make sure this works right
+            // mockProductService.Setup(e => e.Delete(It.IsAny<Product>())); -- does this not work with void return types??
+
+            mockProductService.Setup(e => e.ValidateAdd(It.IsAny<Product>())).CallBase();
+            mockProductService.Setup(e => e.ValidateUpdate(It.IsAny<Product>())).CallBase();
+            mockProductService.Setup(e => e.ValidateDelete(It.IsAny<Product>())).CallBase();
+
+
+
+            productService = mockProductService.Object;
         }
+
+        #endregion
 
         [TestMethod]
         [Description("GenericService<T>.New() method should return transient instance of type T")]
         public void NewMethod_Returns_TransientOfExpectedType()
         {
-            // setup our fake services
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            var productService = new GenericService<Product>(mockProductRepo.Object);
-            var mockFeatureRepo = new Mock<IRepository<ProductFeature>>();
-            var featureService = new GenericService<ProductFeature>(mockFeatureRepo.Object);
-
             // make sure the repositories are only returning the expected types
-            var p = productService.New();
+            var p = productService.New();            
             Assert.IsInstanceOfType(p, typeof(Product));
             Assert.IsNotInstanceOfType(p, typeof(ProductFeature));
-            Assert.IsTrue(p.IsTransient());
-
-            var f = featureService.New();
-            Assert.IsInstanceOfType(f, typeof(ProductFeature));
-            Assert.IsNotInstanceOfType(f, typeof(Product));
-            Assert.IsTrue(f.IsTransient());            
+            Assert.IsTrue(p.IsTransient());    
         }
 
         #region Retrieve Data Tests
@@ -66,11 +92,12 @@ namespace FossLock.Test.BLL
         #region .Add(T entity) Tests
 
         [TestMethod]
-        [Description("GenericService<T>.Add() should throw ArgumentNullException is a null entity is provided.")]
+        [Description("GenericService<T>.Add() should throw ArgumentNullException is " +
+            "a null entity is provided.  Underlying repository .Add should never get called.")]
         public void AddMethod_NullEntity_ThrowsException()
         {
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            var productService = new GenericService<Product>(mockProductRepo.Object);
+            var repoAddCalls = productRepoCalls["Add"];
+
             try
             {
                 productService.Add(null);
@@ -80,57 +107,20 @@ namespace FossLock.Test.BLL
             {
                 Assert.IsInstanceOfType(ex, typeof(ArgumentNullException));
                 Assert.AreEqual("entity", ((ArgumentNullException)ex).ParamName);
+
+                // make sure underlying repository .Add hasn't been called.
+                Assert.AreEqual(repoAddCalls, productRepoCalls["Add"]);
             }
         }
 
         [TestMethod]
-        [Description("Add method can only be called on transient entities.")]
-        public void AddMethod_NonTransientEntity_ThrowsException() 
+        [Description("Add method should throw an exception if any validation " +
+            " failures on entity.  Underlying repository .Add should never get called.")]
+        public void AddMethod_NonValidEntity_ThrowsException()
         {
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            var productService = new GenericService<Product>(mockProductRepo.Object);
+            var repoAddCalls = productRepoCalls["Add"];
 
-            /* hack: IsTransient() is just based on whether or not our entity has an id.. 
-             * so we just sent an Id to make it think our entity is in the db
-             * can't use interfaces for our generics because they require concrete types 
-             * and ef won't play nicely with our inheritance chain if we use interfaces for type declarations
-             * could just mark the method as virtual and then we could override it with Moq.
-             */
-            var p = productService.New();
-            p.Id = 1;
-
-            try
-            {
-                productService.Add(p);
-                Assert.Fail("Supply a non-transient entity to GenericService<T>.Add should throw an ArgumentException.");
-            }
-            catch (ArgumentException ex)
-            {
-                // this is what we want
-                Assert.AreEqual("entity", ex.ParamName);
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail("Unexpected exception type: {0}\nMessage: {1}", ex.GetType(), ex.Message);
-
-            }
-        }
-
-        [TestMethod]
-        [Description("Add method should throw an exception if any validation failures on entity.")]
-        public void AddMethod_NonValidEntity_ThrowsException() 
-        {
-            // setup our repository
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            mockProductRepo
-                .Setup(e => e.Add(It.IsAny<Product>()))
-                .Returns((Product entity) => { 
-                    entity.Id = 1; 
-                    return entity; 
-                });            
-
-            var productService = new GenericService<Product>(mockProductRepo.Object);
-
+            // setup a product that fails validation
             var mockProduct = new Mock<Product>();
             mockProduct
                 .Setup(e => e.ValidationResults())
@@ -139,61 +129,50 @@ namespace FossLock.Test.BLL
                     new ValidationResult("Yep.. really didn't validate.")
                 });
 
+
             try
             {
                 productService.Add(mockProduct.Object);
                 Assert.Fail("Expected an ArgumentException but received none.");
             }
-            catch (ArgumentException ex) 
+            catch (Exception ex)
             {
-                Assert.AreEqual("entity", ex.ParamName, true, ex.ToString());
-            }
-            catch (Exception ex) 
-            {
-                Assert.Fail("Unexpected exception type: {0}\n" + 
-                    "Message: {1}", ex.GetType(), ex.Message);
+                Assert.IsInstanceOfType(ex, typeof(ArgumentException));
+                Assert.AreEqual("entity", ((ArgumentException)ex).ParamName);
+
+                // make sure underlying repository .Add hasn't been called.
+                Assert.AreEqual(repoAddCalls, productRepoCalls["Add"]);
             }
         }
 
         [TestMethod]
-        [Description("Add method should complete successfully as long as the ValidateAdd() returns an empty list.")]
-        public void AddMethod_ValidEntity_Succeeds() 
+        [Description("Add method should complete successfully as long as the ValidateAdd() " +
+            "returns an empty list.  Underlying repository .Add should get called one time.")]
+        public void AddMethod_ValidEntity_Succeeds()
         {
-            // setup our repository
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            mockProductRepo
-                .Setup(e => e.Add(It.IsAny<Product>()))
-                .Returns((Product entity) => {
-                    entity.Id = 1;
-                    return entity;
-                });
-            
-            // make sure validation succeeds
-            var mockProductService = new Mock<GenericService<Product>>(mockProductRepo.Object);
-            mockProductService
-                .Setup(e => e.ValidateAdd(It.IsAny<Product>()))
-                .Returns(new List<ValidationResult>());
-            
-            var productService = mockProductService.Object;
+            var repoAddCalls = productRepoCalls["Add"];
 
             var p = productService.New();
+
+            Assert.IsTrue(p.IsTransient());
+
             p = productService.Add(p);
 
-            // this should succeed, and product should now have an id of 1 
-            Assert.AreEqual(1, p.Id);
-
+            Assert.IsTrue(p.IsTransient() == false);
+            Assert.AreEqual((repoAddCalls + 1), productRepoCalls["Add"]);
         }
+
 
         #endregion
 
         #region .Update(T entity) Tests
 
         [TestMethod]
-        [Description("Update method should raise a NullArgumentException if it is passed a null.")]
+        [Description(".Update() should raise a NullArgumentException if it is passed a null. " +
+            "Underlying repository .Update() should not be called.")]
         public void UpdateMethod_NullEntity_ThrowsException()
         {
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            var productService = new GenericService<Product>(mockProductRepo.Object);
+            var repoUpdateCalls = productRepoCalls["Update"];
 
             try
             {
@@ -204,60 +183,32 @@ namespace FossLock.Test.BLL
             {
                 Assert.IsInstanceOfType(ex, typeof(ArgumentNullException));
                 Assert.AreEqual("entity", ((ArgumentNullException)ex).ParamName);
+                Assert.AreEqual(repoUpdateCalls, productRepoCalls["Update"]);
             }
         }
 
         [TestMethod]
-        [Description("Update method can only be called on objects already in the data store.")]
-        public void UpdateMethod_TransientEntity_ThrowsException() 
+        [Description("Update method should throw an exception if the entity reports itself invalid. " +
+            "Underlying repository .Update() should not be called.")]
+        public void UpdateMethod_NonValidEntity_ThrowsException()
         {
-            // setup our repository
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            mockProductRepo
-                .Setup(e => e.Update(It.IsAny<Product>()))
-                .Returns((Product entity) => { return entity; });
+            var repoUpdateCalls = productRepoCalls["Update"];
 
-            var productService = new GenericService<Product>(mockProductRepo.Object);
-            var p = productService.New();
-
-            Assert.IsTrue(p.IsTransient());
-
-            try
-            {
-                productService.Update(p);
-                Assert.Fail("Expected an ArgumentException but none thrown.");
-            }
-            catch (Exception ex) 
-            {
-                Assert.IsInstanceOfType(ex, typeof(ArgumentException));
-                Assert.AreEqual("entity", ((ArgumentException)ex).ParamName);
-            }
-        }
-
-        [TestMethod]
-        [Description("Update method should throw an exception if the entity reports itself invalid.")]
-        public void UpdateMethod_NonValidEntity_ThrowsException() 
-        {
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            mockProductRepo
-                .Setup(e => e.Update(It.IsAny<Product>()))
-                .Returns((Product entity) => { return entity; });
-
-            var productService = new GenericService<Product>(mockProductRepo.Object);
-
-            var mockProduct = new Mock<Product>();            
-            mockProduct
-                .Setup(e => e.ValidationResults())
-                .Returns(new List<ValidationResult> { 
+            var validationErrors = new List<ValidationResult> { 
                     new ValidationResult("Yo dawg, you didn't validate."),
                     new ValidationResult("Yep.. really didn't validate.")
-                });
+                };
+
+            var mockProduct = new Mock<Product>();
+            mockProduct
+                .Setup(e => e.ValidationResults())
+                .Returns(validationErrors);
 
             var p = mockProduct.Object;
-            p.Id = 1;
-
+            p.Id = 1; // anything other than 0 looks like it's in the database
+            
             Assert.AreEqual(false, p.IsTransient());
-            Assert.AreEqual(2, p.ValidationResults().Count);
+            Assert.AreEqual(validationErrors.Count, p.ValidationResults().Count);
 
             try
             {
@@ -268,30 +219,31 @@ namespace FossLock.Test.BLL
             {
                 Assert.IsInstanceOfType(ex, typeof(ArgumentException), ex.ToString());
                 Assert.AreEqual("entity", ((ArgumentException)ex).ParamName);
+                Assert.AreEqual(repoUpdateCalls, productRepoCalls["Update"]);
             }
         }
 
+        // add -- repository IS called
         [TestMethod]
-        [Description("Update method should complete successfully as long as ValidateUpdate() returns an empty list.")]
-        public void UpdateMethod_ValidEntity_Succeeds() 
+        [Description("Update method should complete successfully as long as ValidateUpdate() " + 
+            "returns an empty list. Underlying repository .Update() should be called exactly one time.")]
+        public void UpdateMethod_ValidEntity_Succeeds()
         {
-            var mockProductRepo = new Mock<IRepository<Product>>();
-            mockProductRepo
-                .Setup(e => e.Update(It.IsAny<Product>()))
-                .Returns((Product entity) => { return entity; });
-
-            var productService = new GenericService<Product>(mockProductRepo.Object);
+            var repoUpdateCalls = productRepoCalls["Update"];
 
             var p = productService.New();
             p.Id = 1;
 
-            productService.Update(p);            
+            productService.Update(p);
+
+            Assert.AreEqual((repoUpdateCalls + 1), productRepoCalls["Update"]);
         }
 
         #endregion
 
         #region .Delete(T entity) Tests
 
+        // add -- repository not called
         [TestMethod]
         [Description("Not yet implemented!")]
         public void DeleteMethod_NullEntity_ThrowsException()
@@ -299,28 +251,79 @@ namespace FossLock.Test.BLL
             Assert.Inconclusive("not implemented");
         }
 
+        // add -- repository not called
         [TestMethod]
         [Description("Not yet implemented!")]
-        public void DeleteMethod_TransientEntity_ThrowsException() 
-        { 
-            Assert.Inconclusive("not implemented."); 
+        public void DeleteMethod_NonValidEntity_ThrowsException()
+        {
+            Assert.Inconclusive("not implemented.");
         }
 
+        // add -- repository IS called
         [TestMethod]
         [Description("Not yet implemented!")]
-        public void DeleteMethod_NonValidEntity_ThrowsException() 
-        { 
-            Assert.Inconclusive("not implemented."); 
-        }
-
-        [TestMethod]
-        [Description("Not yet implemented!")]
-        public void DeleteMethod_ValidEntity_Succeeds() 
-        { 
+        public void DeleteMethod_ValidEntity_Succeeds()
+        {
             Assert.Inconclusive("not implemented.");
         }
 
         #endregion
+
+
+        /*
+         * For the following method calls, if the function returns an 
+         * empty list, that is considered successful validation.  Null
+         * returns should never occur, and exceptions should not be thrown here.
+         */
+
+        #region ValidateAdd Tests
+
+        public void ValidateAdd_NonTransientEntity_Fails()
+        { }
+
+        public void ValidateAdd_TransientEntity_Succeeds()
+        { }
+
+        public void ValidateAdd_InvalidEntity_Fails()
+        { }
+
+        public void ValidateAdd_ValidTransient_Succeeds()
+        { }
+
+        #endregion
+
+        #region ValidateUpdate Tests
+
+        public void ValidateUpdate_TransientEntity_Fails()
+        { }
+
+        public void ValidateUpdate_NonTransient_Succeeds()
+        { }
+
+        public void ValidateUpdate_InvalidEntity_Fails()
+        { }
+
+        public void ValidateUpdate_NonTransientValid_Succeeds()
+        { }
+
+        #endregion
+
+        #region ValidateDelete Tests
+
+        public void ValidateDelete_TransientEntity_Fails()
+        { }
+
+        public void ValidateDelete_NonTransient_Succeeds()
+        { }
+
+        // validation errors at the entity level should not have any impact on delete 
+        // succeeding.. for constraints, like "You can't delete this entity because --whatever-- reason",
+        // this should be enforced by overriding ValidateDelete in a derrived class.
+        public void ValidateDelete_InvalidEntity_Succeeds()
+        { }
+
+        #endregion
+
 
     }
 }
