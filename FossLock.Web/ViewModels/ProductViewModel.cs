26d30856c6b53d05067e3508a8cbbbe4f4fea9b0
Derek J. Curtis <djcurtis@summersetsoftware.com>
2014-07-07 15:13:49 +0700

    Changed ProductViewModel to initialize all it's reference properties in the constructor (previously was inconsistent.. some but not all list properties were initialized automatically by their getters).  Ignored *.css files from git.

--- FossLock.Web/ViewModels/ProductViewModel.cs
+++ FossLock.Web/ViewModels/ProductViewModel.cs
@@ -10,8 +10,64 @@ namespace FossLock.Web.ViewModels
 {
     public class ProductViewModel
     {
+        /// <summary>
+        ///     Initializes the ProductViewModel.
+        ///     Any reference types, specifically lists, that require instantiation,
+        ///     are done so by the constructor (so no properties should ever return null).
+        /// </summary>
         public ProductViewModel()
         {
+            // initialize our lists
+            SelectedDefaultLockProperties = new List<LockPropertyType>();
+            PermittedActivationTypes = new List<ActivationType>();
+
+            // the following 4 properties are used in the Razor templates to display
+            // all the possible choices.  Currently all four are based on enums (but not
+            // all the enum values are displayed).
+            AllLockProperties = ((IEnumerable<int>)Enum.GetValues(typeof(LockPropertyType)))
+                .Where(e => (LockPropertyType)e != LockPropertyType.None)
+                .Select(e => new SelectListItem
+                {
+                    Text = Enum.GetName(typeof(LockPropertyType), e),
+                    Value = e.ToString()
+                });
+
+            AllActivationTypes = ((IEnumerable<int>)Enum.GetValues(typeof(ActivationType)))
+                .Where(e =>
+                {
+                    // the following types are not currently permitted and shouldn't be shown
+                    if ((ActivationType)e != ActivationType.None &&
+                        (ActivationType)e != ActivationType.Phone &&
+                        (ActivationType)e != ActivationType.SMS)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                })
+                .Select(e => new SelectListItem
+                {
+                    Text = Enum.GetName(typeof(ActivationType), e),
+                    Value = e.ToString()
+                });
+
+            AllVersioningStyles = ((IEnumerable<int>)Enum.GetValues(typeof(VersioningStyle)))
+                .Select(e => new SelectListItem
+                {
+                    Text = Enum.GetName(typeof(VersioningStyle), e),
+                    Value = e.ToString()
+                });
+
+            AllLeewayTypes = ((IEnumerable<int>)Enum.GetValues(typeof(VersionLeewayType)))
+                .Select(e => new SelectListItem
+                {
+                    Text = Enum.GetName(typeof(VersionLeewayType), e),
+                    Value = e.ToString()
+                });
+
+            // set our default values
         }
 
         #region Basics
@@ -43,117 +99,26 @@ namespace FossLock.Web.ViewModels
         public IList<LockPropertyType> SelectedDefaultLockProperties { get; set; }
 
         [Display(Name = "Licensing fails on null hardware identifier.")]
-        [DefaultValue(true)]
         public bool FailOnNullHardwareIdentifier { get; set; }
 
         [Display(Name = "Allowed Activation Modes")]
         public IList<ActivationType> PermittedActivationTypes { get; set; }
 
-        [Display(Name = "Version Leeway")]
         [Required]
-        [DefaultValue(VersionLeewayType.WithinSameMajorVersion)]
+        [Display(Name = "Version Leeway")]
         public VersionLeewayType VersionLeeway { get; set; }
 
         #endregion License/Security
 
         #region Selection Lists
 
-        private IEnumerable<SelectListItem> _allLockProeprties = null;
+        public IEnumerable<SelectListItem> AllLockProperties { get; private set; }
 
-        public IEnumerable<SelectListItem> AllLockProperties
-        {
-            get
-            {
-                if (_allLockProeprties == null)
-                {
-                    _allLockProeprties =
-                        ((IEnumerable<int>)Enum.GetValues(typeof(LockPropertyType)))
-                        .Where(e => (LockPropertyType)e != LockPropertyType.None)
-                        .Select(e => new SelectListItem
-                        {
-                            Text = Enum.GetName(typeof(LockPropertyType), e),
-                            Value = e.ToString()
-                        });
-                }
-                return _allLockProeprties;
-            }
-        }
+        public IEnumerable<SelectListItem> AllActivationTypes { get; private set; }
 
-        private IEnumerable<SelectListItem> _allActivationTypes = null;
+        public IEnumerable<SelectListItem> AllVersioningStyles { get; private set; }
 
-        public IEnumerable<SelectListItem> AllActivationTypes
-        {
-            get
-            {
-                if (_allActivationTypes == null)
-                {
-                    _allActivationTypes = ((IEnumerable<int>)Enum.GetValues(typeof(ActivationType)))
-                        .Where(e =>
-                        {
-                            var canUse = false;
-
-                            // the following types are not currently permitted and shouldn't be shown
-                            if ((ActivationType)e != ActivationType.None &&
-                                (ActivationType)e != ActivationType.Phone &&
-                                (ActivationType)e != ActivationType.SMS)
-                            {
-                                canUse = true;
-                            }
-
-                            return canUse;
-                        })
-                        .Select(e => new SelectListItem
-                        {
-                            Text = Enum.GetName(typeof(ActivationType), e),
-                            Value = e.ToString()
-                        });
-                }
-
-                return _allActivationTypes;
-            }
-        }
-
-        private IEnumerable<SelectListItem> _allVersioningStyles = null;
-
-        public IEnumerable<SelectListItem> AllVersioningStyles
-        {
-            get
-            {
-                if (_allVersioningStyles == null)
-                {
-                    _allVersioningStyles =
-                        ((IEnumerable<int>)Enum.GetValues(typeof(VersioningStyle)))
-                        .Select(e => new SelectListItem
-                        {
-                            Text = Enum.GetName(typeof(VersioningStyle), e),
-                            Value = e.ToString()
-                        });
-                }
-
-                return _allVersioningStyles;
-            }
-        }
-
-        private IEnumerable<SelectListItem> _allLeewayTypes = null;
-
-        public IEnumerable<SelectListItem> AllLeewayTypes
-        {
-            get
-            {
-                if (_allLeewayTypes == null)
-                {
-                    _allLeewayTypes =
-                        ((IEnumerable<int>)Enum.GetValues(typeof(VersionLeewayType)))
-                        .Select(e => new SelectListItem
-                        {
-                            Text = Enum.GetName(typeof(VersionLeewayType), e),
-                            Value = e.ToString()
-                        });
-                }
-
-                return _allLeewayTypes;
-            }
-        }
+        public IEnumerable<SelectListItem> AllLeewayTypes { get; private set; }
 
         #endregion Selection Lists
     }
