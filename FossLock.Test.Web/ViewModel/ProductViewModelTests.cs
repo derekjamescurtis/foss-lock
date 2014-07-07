a481944ca50362d19308bbec1a62aa72b8cf626d
Derek J. Curtis <djcurtis@summersetsoftware.com>
2014-07-07 15:14:22 +0700

    Changed ProductViewModel to initialize all it's reference properties in the constructor (previously was inconsistent.. some but not all list properties were initialized automatically by their getters).  Ignored *.css files from git.

---
+++ FossLock.Test.Web/ViewModel/ProductViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace FossLock.Test.Web.ViewModel
+{
+    [TestFixture]
+    internal class ProductViewModelTests
+    {
+        public void ViewModel_ListProperties_Initialized()
+        {
+            // make sure all the list-type properties of the viewmodel are initialized properly.
+        }
+    }
+}
