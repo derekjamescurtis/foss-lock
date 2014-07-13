010d16b5ef3457f3ee1e8f0aed8b7a7dae85c4fc
Derek J. Curtis <djcurtis@summersetsoftware.com>
2014-07-13 17:02:30 +0700

    Modified initialization test case slightly -- using reflection to lookup reference type properties instead of hardcoding them

--- FossLock.Test.Web/ViewModel/CustomerViewModelTests.cs
+++ FossLock.Test.Web/ViewModel/CustomerViewModelTests.cs
@@ -13,18 +13,31 @@ namespace FossLock.Test.Web.ViewModel
     {
         private CustomerViewModel vm = null;
 
+        /// <summary>
+        ///     Reinitializes our viewmodel prior to each test.
+        /// </summary>
         [SetUp]
         public void SetUp()
         {
             vm = new CustomerViewModel();
         }
 
+        /// <summary>
+        ///     Ensures that our complex-type references are all initialized automatically
+        ///     in our viewmodels.
+        /// </summary>
         [Test]
         public void ViewModel_ComplexTypes_AreInitialized()
         {
-            Assert.IsNotNull(vm.StreetAddress);
-            Assert.IsNotNull(vm.BillingAddress);
-            Assert.IsNotNull(vm.PrimaryContact);
+            var referenceTypes = typeof(CustomerViewModel).GetProperties()
+                .Where(e =>
+                    e.PropertyType != typeof(System.String) &&
+                    e.PropertyType.IsValueType == false
+                );
+            foreach (var propInfo in referenceTypes)
+            {
+                Assert.IsNotNull(propInfo.GetValue(vm));
+            }
         }
     }
 }
